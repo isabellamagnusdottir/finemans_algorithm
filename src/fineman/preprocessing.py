360e84e4cae859e96486aa4a8a9b28f908db49b8
imag6 <isabellahmagnusdottir@gmail.com>
2025-02-06 20:04:53 +0100

    Initial unit tests of preprocessing
    Initial draft of unit test for fineman/preprocessing.py, and additional comments in implementation,
    as well as graphs in JSON format in test_data folder.

--- src/fineman/preprocessing.py
+++ src/fineman/preprocessing.py
@@ -53,31 +53,6 @@ def ensure_max_degree(graph: dict[int, set[tuple[int, int]]], n: int, threshold:
             split_queue.append(new_vertex1)
         if len(graph[new_vertex2]) > threshold:
             split_queue.append(new_vertex2)
+        # TODO: consider if it even happens that the first one does not, but the second does?
 
     return graph
-
-
-
-def main():
-    graph = {
-        1: {(2,1), (3,-1)}
-    }
-    neg_vertices = [1]
-    new_graph = ensure_neg_vertices_has_degree_of_one(graph, neg_vertices, 3)
-    print(new_graph)
-
-    graph = {
-        1: {(2,1), (3,1), (4,1)}
-    }
-    new_graph = ensure_max_degree(graph, 4, 2)
-    print(new_graph)
-
-    graph = {
-        1: {(2,1), (3,1), (4,1), (5,1), (6,1), (7,1), (8,1)}
-    }
-    new_graph = ensure_max_degree(graph, 8, 3)
-    print(new_graph)
-
-
-if __name__ == "__main__":
-    main()
---
+++ src/tests/test_preprocessing.py
@@ -0,0 +1,78 @@
+import pytest
+from fineman.preprocessing import *
+from utils import load_test_case
+
+TESTDATA_FILEPATH = "src/tests/test_data/"
+
+# DEGREE OF ONE TESTS
+def test_ensure_degree_of_one_for_tree_with_neg_root():
+    graph = load_test_case(TESTDATA_FILEPATH + "tree_graph_two_layered_negative_root.json")
+    assert len(graph[1]) == 2
+
+    graph = ensure_neg_vertices_has_degree_of_one(graph, [1], len(graph.keys()))
+    assert len(graph[1]) == 1
+
+def test_ensure_same_graph_on_graph_with_no_neg_edges():
+    graph = load_test_case(TESTDATA_FILEPATH + "complete_four_vertices_graph_with_no_neg_edges.json")
+    new_graph = ensure_neg_vertices_has_degree_of_one(graph, [], len(graph.keys()))
+
+    assert graph == new_graph
+
+def test_graph_already_adhere_to_one_degree_restriction():
+    graph = load_test_case(TESTDATA_FILEPATH + "graph_already_adhere_to_one_degree_restriction.json")
+    new_graph = ensure_neg_vertices_has_degree_of_one(graph, [2], len(graph.keys()))
+
+    assert graph == new_graph
+
+
+
+
+# DEGREE OF AT MOST THE THRESHOLD TESTS
+@pytest.mark.parametrize("filename,threshold", [("tree_graph_two_layered_negative_root.json", 2), ("graph_already_adhere_to_one_degree_restriction.json", 2)])
+def test_maxdegree_already_ensured(filename, threshold):
+    graph = load_test_case(TESTDATA_FILEPATH + filename)
+    new_graph = ensure_max_degree(graph, len(graph.keys()), threshold)
+    
+    assert graph ==  new_graph
+
+
+def test_complete_graph_is_doubled():
+    graph = load_test_case(TESTDATA_FILEPATH + "complete_four_vertices_graph_with_no_neg_edges.json")
+    threshold = 2
+    graph = ensure_max_degree(graph, len(graph.keys()), threshold)
+
+    for _, v in graph.items():
+        assert len(v) <= threshold
+    
+    assert len(graph.keys()) == 12
+
+def test_one_split_not_enough():
+    graph = load_test_case(TESTDATA_FILEPATH + "tree_graph_single_root_with_7_children.json")
+    threshold = 3
+    graph = ensure_max_degree(graph, len(graph.keys()), threshold)
+
+    for _, v in graph.items():
+        assert len(v) <= threshold
+    
+    assert len(graph.keys()) == 12
+
+def test_two_splits_required_on_both_sides():
+    graph = load_test_case(TESTDATA_FILEPATH + "tree_graph_single_root_with_10_children.json")
+    threshold = 3
+    graph = ensure_max_degree(graph, len(graph.keys()), threshold)
+
+    for _, v in graph.items():
+        assert len(v) <= threshold
+    
+    assert len(graph.keys()) == 17
+
+@pytest.mark.parametrize("threshold,expected", [(50, 103), (15, 115), (10, 131)])
+def test_multiple_splits_on_hundred_vertex_graph(threshold, expected):
+    graph = load_test_case(TESTDATA_FILEPATH + "tree_graph_single_root_with_100_children.json")
+    graph = ensure_max_degree(graph, len(graph.keys()), threshold)
+
+    for _, v in graph.items():
+        assert len(v) <= threshold
+    
+    assert len(graph.keys()) == expected
+
