fbc188df47f1bd7c5f9eb2bdc0c35fa929134e3f
imag6 <isabellahmagnusdottir@gmail.com>
2025-02-06 20:03:22 +0100

    Added functionality to support unit tests
    Added graph loader and export of functionality to make unittesting possible
    
    Co-authored-by: Kobo-coder <jakob.hjalgrim@gmail.com>

--- src/utils/load_test_case.py
+++ src/utils/load_test_case.py
@@ -1,4 +1,5 @@
 import json
+import os
 
 def load_test_case(path):
 
@@ -8,3 +9,9 @@ def load_test_case(path):
     graph = { int(k): set(tuple(edge) for edge in v) for k, v in data.items() }
 
     return graph
+
+def main():
+    print(os.getcwd())
+
+if __name__ == "__main__":
+    main()
