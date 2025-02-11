6cb5efb2f87bfa499d856dccdc1bc7f0857be584
Jakob <73228675+Kobo-coder@users.noreply.github.com>
2025-02-11 12:34:37 +0100

    Finish step 3,4,5 of Fineman's algorithm
    Added functionality for the 3rd, 4th and 5th step of
    Fineman's algorithm.
    Added functionality to find a 1-hop independent set of
    a negative subset, or find one half of a negative
    sandwich's crust.
    Added functionality to compute reach, subset BFD, BFD
    where rounds are saved.
    Also added negative cycle check when finding a random
    1-hop independent set.
    
    Added tests for new functions.

---
+++ src/fineman/independent_set_or_crust.py
@@ -0,0 +1,29 @@
+from fineman.helper_functions import *
+from fineman.rand_is import *
+from fineman.heavy_light_partition import *
+import time
+import math
+import random as rand
+
+
+def find_is_or_crust(graph,neg_edges,negative_subset,c, c_prime, seed = None):
+    if seed is not None: rand.seed(seed)
+    k_hat = len(negative_subset)
+    rho = k_hat**(1/3)
+    (H,L) = heavy_light_partition(graph,neg_edges,negative_subset,rho,c,seed)
+    if H:
+        y = rand.choice(tuple(H))
+        #TODO: Consider if we want to strictly extract the transpose graph here as we will
+        # be working with the transpose graph after if we succeed in finding large enough set
+        dist = b_hop_stsp(y,graph,1)
+        U = {u for u in negative_subset if dist[u] < 0}
+        if len(U) < (1/8)*k_hat*rho:
+            return find_is_or_crust(graph,neg_edges,negative_subset,c,(int(time.time()*1000)))
+        else:
+            return (y,U)
+    else:
+        # TODO: argue for the value of c' since the choice varies probability of success
+        IS_size_threshold = rho/16
+        for _ in range(c_prime*math.ceil(np.log2(len(graph)))):
+            I = rand_is(graph,neg_edges,L,rho)
+            if len(I) >= IS_size_threshold: return I
---
+++ src/tests/test_rand_is.py
@@ -0,0 +1,14 @@
+import pytest
+from fineman import rand_is
+from utils.load_test_case import load_test_case
+
+TESTDATA_FILEPATH = "src/tests/test_data/graphs/"
+
+@pytest.mark.parametrize("repeat", range(10))  # Repeat 10 times
+@pytest.mark.parametrize("subset,expected",[
+    ({0},({0}))
+])
+def test_random_independent_set_for_large_weight_cycle(subset,expected,repeat):
+    graph,neg_edges = load_test_case(TESTDATA_FILEPATH+"6_cycle_large_positive_weights.json")
+    actual = rand_is(graph,neg_edges,subset,1)
+    assert actual == expected
