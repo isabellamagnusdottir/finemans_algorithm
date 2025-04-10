d4ded168c9c351f55f55d944f880af8e21ac1896
imag6 <isabellahmagnusdottir@gmail.com>
2025-04-10 10:19:54 +0200

    Refactored Fineman's algorithm s.t. it doesnot check for connected components.
    Moved the responsibility for checking if the graph is a connected component from
    Fineman's algorithm to load_test_files.

--- src/utils/load_test_case.py
+++ src/utils/load_test_case.py
@@ -1,8 +1,37 @@
 import json
 import os
+from collections import deque
 
+def _find_connected_component_to_source(graph, source: int):
+    mapping = {}
 
-def load_test_case(path: str) -> tuple[dict[int, dict[int, int]], set[tuple[int, int]]]:
+    queue = deque()
+    explored = [False] * len(graph)
+    mapping[source] = 0
+    queue.append(source)
+    explored[source] = True
+
+    new_graph = {0: {}}
+    neg_edges = set()
+
+    while queue:
+        vertex = queue.popleft()
+        if mapping[vertex] not in new_graph:
+            new_graph[mapping[vertex]] = {}
+
+        for n, w in graph[vertex].items():
+            if not explored[n]:
+                new_name = len(mapping)
+                mapping[n] = new_name
+                queue.append(n)
+                explored[n] = True
+            new_graph[mapping[vertex]][mapping[n]] = w
+            if w < 0:
+                neg_edges.add((mapping[vertex], mapping[n]))
+
+    return new_graph, neg_edges
+
+def load_test_case(path: str, only_cc = False) -> tuple[dict[int, dict[int, int]], set[tuple[int, int]]]:
 
     with open(path, 'r') as file:
         data = json.load(file)
@@ -21,6 +50,8 @@ def load_test_case(path: str) -> tuple[dict[int, dict[int, int]], set[tuple[int,
             if weight < 0:
                 neg_set.add((vertex, neighbor))
 
+    if only_cc:
+        return _find_connected_component_to_source(graph, 0)
     return graph, neg_set
 
 def main():
